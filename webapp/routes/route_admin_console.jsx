00ece5f87db529f00fe2014bf5989525f1f00706
Aymane Khouaji <akhouaji@linagora.com>
2016-10-06 09:18:32 +0200

    Add the ldapSetting in admin menu

--- webapp/routes/route_admin_console.jsx
+++ webapp/routes/route_admin_console.jsx
@@ -16,6 +16,7 @@ import EmailAuthenticationSettings from 'components/admin_console/email_authenti
 import GitLabSettings from 'components/admin_console/gitlab_settings.jsx';
 import OAuthSettings from 'components/admin_console/oauth_settings.jsx';
 import LdapSettings from 'components/admin_console/ldap_settings.jsx';
+import GoLdapSettings from 'components/admin_console/go_ldap.jsx';
 import SamlSettings from 'components/admin_console/saml_settings.jsx';
 import ClusterSettings from 'components/admin_console/cluster_settings.jsx';
 import MetricsSettings from 'components/admin_console/metrics_settings.jsx';
@@ -100,6 +101,10 @@ export default (
             <Route
                 path='ldap'
                 component={LdapSettings}
+            />
+            <Route
+                path='go_ldap'
+                component={GoLdapSettings}
             />
             <Route
                 path='saml'
